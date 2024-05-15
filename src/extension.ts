dcb411e87236698bb6ba0588680066e7e1d65598
daniel.biro <daniel.biro@stud.ubbcluj.ro>
2024-05-15 08:59:59 +0300

    M I K L Ó S works, added picture and rendering HTML

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('miklos.start', () => {
+			
+			const panel = vscode.window.createWebviewPanel(
+				'miklos',
+				'M I K L Ó S',
+				vscode.ViewColumn.One,
+				{
+					localResourceRoots: [context.extensionUri]
+				}
+			);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "miklos-extension" is now active!');
+			let imagePath = panel.webview.asWebviewUri(vscode.Uri.joinPath(context.extensionUri, 'img', 'miklos.jpg'));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('miklos-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from M I K L Ó S!');
-	});
-
-	context.subscriptions.push(disposable);
+			panel.webview.html = getWebViewContent(imagePath);
+		})
+	);
 }
 
-// This method is called when your extension is deactivated
+function getWebViewContent(imagePath: vscode.Uri) {
+	return `
+		<!DOCTYPE html>
+		<html lang="en>
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>M I K L Ó S</title>
+			</head>
+			<body>
+				<img src="${imagePath}" />
+			</body>
+		</html>
+	`;
+}
+
 export function deactivate() {}
